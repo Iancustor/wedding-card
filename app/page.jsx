8bb2b762c770ff89a72ae5e23258efe925354731
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-02-23 10:16:45 +0300

    updated card

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import imageEl from "../public/images/watsup.png";
-import imageCard from "../public/images/card.jpg";
+import imageCard from "../public/images/card.png";
 import Link from "next/link";
 import { generateImageUrl } from "./utils/generateImageUrl";
 import { useRouter } from "next/navigation";
@@ -11,36 +11,47 @@ function page() {
   const [name, setName] = useState("");
   const [contact, setContact] = useState("");
   const [generated, setGenerated] = useState(false);
-  // const imageUrl = generateImageUrl(imageCard);
-  const imageUrl = "https://res.cloudinary.com/dp0tf8poo/image/upload/v1708624122/gakor8kcbm19i0zf9qf3.jpg";
-const router =useRouter()
+
+  const imageUrl =
+    "https://res.cloudinary.com/dp0tf8poo/image/upload/v1708669141/bse9rbzizzo4rktkkoue.png";
+  const router = useRouter();
   function handleSubmition() {
-    const message=`
-    Dear ${name},\n\n We are thrilled to extend our heartfelt invitation to you for the joyous wedding celebration\n of Mr. John and Mrs. Jane, which will take place on Sunday, 20th August 2024. Your presence\n would add immense joy to this special day. Please find attached the invitation card for further details \n${imageUrl}`;
-    console.log(message)
+    const message = `
+    Dear ${name},\n\n We are thrilled to extend our heartfelt invitation to you for the joyous wedding celebration\n of Mr.Joel Isaac
+    Kakembo  and Mrs.Vanessa Ssekajja, which will take place on , 2nd of
+    March 2024. Your presence\n would add immense joy to this special day. Please find attached the invitation card for further details \n${imageUrl}`;
+    // console.log(message);
     const encodedMessage = encodeURIComponent(message);
-    const whatsappLink=`https://wa.me/${contact}?text=${encodedMessage}`;
+    const whatsappLink = `https://wa.me/${contact}?text=${encodedMessage}`;
 
-    router.push(whatsappLink)
-    console.log(name, contact);
+    router.push(whatsappLink);
+    // console.log(name, contact);
 
-    console.log(whatsappLink);
-    setGenerated(true);
     setContact("");
     setName("");
+    setGenerated(validateInput(contact));
   }
-
+  function validateInput() {
+    if (generated) {
+      // Your submission logic here
+      console.log("Form submitted with valid input:", contact);
+    } else {
+      console.log("Form not submitted. Please provide valid input.");
+    }
+  }
   return (
-    <div className="flex lg:flex-row flex-col min-h-screen justify-center  bg-red-100 ">
+    <div className="flex lg:flex-row flex-col min-h-screen lg:px-56 py-4 justify-center  bg-red-100 ">
       <div className="p-4 order-2 lg:order-1 w-full lg:w-1/2">
-        <div className=" bg-red-500 flex items-center justify-center  px-5 py-4 rounded ">
+        <div className="flex items-center justify-center  px-2 py-4 rounded ">
           <Image src={imageCard} alt="" width={300} height={350} />
         </div>
       </div>
       <div className="gap-4 flex flex-col p-4 max-w-4xl lg:order-2 order-1 w-full lg:w-1/2">
         <div className="flex gap-4 items-center justify-center flex-col ">
-          <h2 className="font-extrabold text-3xl">
-            Joel Isaac Kakembo & Vanessa Ssekajja
+          <h2 className="font-extrabold text-3xl flex  font-sans">
+            Joel Isaac Kakembo <br /> <span className="text-red-600 ">&</span>{" "}
+            <br />
+            Vanessa Ssekajja
           </h2>
           {/* {generated && (
             <Link
@@ -56,9 +67,7 @@ const router =useRouter()
             </Link>
           )} */}
         </div>
-        <div
-          className=" px-7 bg-red-200 py-4 rounded-t-xl"
-        >
+        <div className=" px-7 bg-red-200 py-4 rounded-t-xl">
           <div className="mb-6">
             <label className="block mb-2 text-sm font-medium text-gray-900">
               Name
@@ -84,25 +93,17 @@ const router =useRouter()
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
             />
           </div>
-          {/* <div className="mb-6">
-            <label className="block mb-2 text-sm font-medium text-gray-900">
-              Email
-            </label>
-            <input
-              value={email}
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5"
-            />
-          </div> */}
+
           <div>
             <button
-            onClick={handleSubmition}
+              onClick={handleSubmition}
               type="button"
-              className="text-black  hover:text-white border bg-white hover:border-transparent hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+              className=" flex items-center hover:bg-black hover:text-red-50  text-black   hover:border-transparent bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-6 justify-center  text-center me-2 mb-2 "
             >
-              Send Invite
+              <div className="">
+                <Image src={imageEl} alt="" className="w-12 h-10 " />
+              </div>
+              Share
             </button>
           </div>
         </div>
